31a38bdab316028534e351ccdedafc3f2d361993
Rahul Sabinkar <rahulsabinkar4u@gmail.com>
2022-02-18 22:32:33 +0530

    initial setup

---
+++ main.js
@@ -0,0 +1,9 @@
+let playground = document.getElementsByClassName("playground");
+let apple = "🍎";
+let field = [
+    {apple, apple, apple, apple, apple},
+    {apple, apple, apple, apple, apple},
+    {apple, apple, apple, apple, apple},
+    {apple, apple, apple, apple, apple},
+    {apple, apple, apple, apple, apple},
+];
